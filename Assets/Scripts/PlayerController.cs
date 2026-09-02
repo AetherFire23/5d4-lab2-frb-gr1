fbf08923526fe593641a81e495dc5eb3ad7b602a
Frederik Richer Balthazard <2410658@cegeplimoilou.ca>
2026-09-02 15:42:19 -0400

    Progress

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravityModifier;
+    public float jumpForce;
+    private Rigidbody rb;
+    private bool isOnGround = false;
+
+    public PlayerController(Rigidbody rb)
+    {
+        this.rb = rb;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, -9.81f * gravityModifier, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            this.rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            this.isOnGround = false;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
