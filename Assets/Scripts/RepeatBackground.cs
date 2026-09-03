13217151dcfa8e86ad5e79355ddd89535442bf7d
2410658 <2410658@cegeplimoilou.ca>
2026-09-03 16:26:07 -0400

    Add step 13

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -2,18 +2,22 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private BoxCollider boxCollider;
     private Vector3 startPos;
+    private float repeatWidth = -1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        this.boxCollider = this.GetComponent<BoxCollider>();
+        this.repeatWidth = this.repeatWidth = boxCollider.size.x / 2;
         this.startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - 50)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
